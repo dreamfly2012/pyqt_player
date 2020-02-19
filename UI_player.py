b1c13f942e12953911f4d09e0898b63624280369
menghuiguli <jia.fu2013@gmail.com>
2020-02-19 15:37:49 +0800

    add playlist and prev and next function

--- UI_player.py
+++ UI_player.py
@@ -14,25 +14,53 @@ class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(587, 447)
+        Form.setStyleSheet("")
         self.playbtn = QtWidgets.QPushButton(Form)
-        self.playbtn.setGeometry(QtCore.QRect(10, 400, 75, 23))
+        self.playbtn.setGeometry(QtCore.QRect(90, 400, 31, 31))
+        self.playbtn.setStyleSheet("#playbtn{\n"
+"    border-radius:15px;\n"
+"background-color:red;\n"
+"    font: 10pt \"楷体\";\n"
+"color:#fff;\n"
+"}")
         self.playbtn.setObjectName("playbtn")
         self.horizontalSlider = QtWidgets.QSlider(Form)
-        self.horizontalSlider.setGeometry(QtCore.QRect(170, 400, 160, 19))
+        self.horizontalSlider.setGeometry(QtCore.QRect(240, 400, 160, 19))
         self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
         self.horizontalSlider.setObjectName("horizontalSlider")
         self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(220, 170, 75, 23))
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 75, 23))
         self.pushButton.setObjectName("pushButton")
+        self.listView = QtWidgets.QListView(Form)
+        self.listView.setGeometry(QtCore.QRect(20, 90, 131, 241))
+        self.listView.setObjectName("listView")
+        self.durantionSlider = QtWidgets.QSlider(Form)
+        self.durantionSlider.setGeometry(QtCore.QRect(30, 360, 501, 19))
+        self.durantionSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.durantionSlider.setObjectName("durantionSlider")
+        self.lcdNumber = QtWidgets.QLCDNumber(Form)
+        self.lcdNumber.setGeometry(QtCore.QRect(240, 150, 281, 121))
+        self.lcdNumber.setObjectName("lcdNumber")
+        self.prevbtn = QtWidgets.QPushButton(Form)
+        self.prevbtn.setGeometry(QtCore.QRect(0, 400, 75, 23))
+        self.prevbtn.setObjectName("prevbtn")
+        self.nextbtn = QtWidgets.QPushButton(Form)
+        self.nextbtn.setGeometry(QtCore.QRect(130, 400, 75, 23))
+        self.nextbtn.setObjectName("nextbtn")
 
         self.retranslateUi(Form)
         self.playbtn.clicked.connect(Form.play)
         self.horizontalSlider.valueChanged['int'].connect(Form.valuechanged)
         self.pushButton.clicked.connect(Form.fileopen)
+        self.nextbtn.released.connect(Form.next)
+        self.prevbtn.released.connect(Form.prev)
+        self.durantionSlider.sliderMoved['int'].connect(self.durantionSlider.setValue)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "音乐播放器by梦回故里"))
-        self.playbtn.setText(_translate("Form", "播放"))
+        self.playbtn.setText(_translate("Form", "||"))
         self.pushButton.setText(_translate("Form", "添加文件+"))
+        self.prevbtn.setText(_translate("Form", "<"))
+        self.nextbtn.setText(_translate("Form", ">"))
--- player.py
+++ player.py
@@ -1,19 +1,27 @@
 from UI_player import Ui_Form
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent,QMediaPlaylist
 from PyQt5.Qt import QUrl,QDir
-from PyQt5.QtWidgets import QMainWindow,QFileDialog
+from PyQt5.QtWidgets import QMainWindow,QFileDialog,QMessageBox
+from PyQt5.QtCore import QStringListModel
+import os
 
 class Player(QMainWindow,Ui_Form):
     def __init__(self, parent=None):
         super(Player, self).__init__()
         self.setupUi(self)
+        self.player = QMediaPlayer()
+        self.currentIndex = 1
+        self.cwd = os.getcwd() # 获取当前程序文件位置
+        self.qList = []
         self.playlist = QMediaPlaylist()
+        self.listView.clicked.connect(self.clickedlist)		 #listview 的点击事件 
         #self.UI_Form.playbtn.clicked.connect(self.play)
         #self.stopbtn.clicked.connect(self.stop)
         
         
-
-
+    #播放器列表添加
+    def add_playlist(self, filename):
+        self.playlist.addMedia(QMediaContent(QUrl.fromLocalFile(filename)))
 
     #初始化播放器列表
     def init_playlist(self,filename):
@@ -31,33 +39,88 @@ class Player(QMainWindow,Ui_Form):
 
         self.player.setPlaylist(self.playlist)
         self.player.setVolume(5)
+        self.play()
          
         
     #控制播放，暂停
     def play(self):
         if self.player.state() == 0 or self.player.state() == 2:
             self.player.play()
-            self.playbtn.setText("播放")
+            self.playbtn.setText("||")
         else:
             self.player.pause()
-            self.playbtn.setText("暂停")        
+            self.playbtn.setText("△")        
         pass
     
     #控制打开文件夹
     def fileopen(self):
-        dlg = QFileDialog()
-        dlg.setFileMode(QFileDialog.AnyFile)
+        files, filetype = QFileDialog.getOpenFileNames(self,  
+                                    "多文件选择",  
+                                    self.cwd, # 起始路径 
+                                    "MP3 Files (*.mp3)")  
+
+        if len(files) == 0:
+            print("\n取消选择")
+            return
+
+        print("\n你选择的文件为:")
+
+        
+        for file in files:
+            
+            self.qList.append(file)
+            
+            self.add_playlist(file)
+            print(file)
+        
+        self.player.setPlaylist(self.playlist)
+        model = QStringListModel(); #创建mode
+        #self.qList = ['Item 1','Item 2','Item 3','Item 4' ]	 #添加的数组数据
+        model.setStringList(self.qList) #将数据设置到model
+        self.listView.setModel(model)##绑定 listView 和 model
+           
+        print("文件筛选器类型: ",filetype)
+
+
+
+        # dlg = QFileDialog()
+        # dlg.setFileMode(QFileDialog.AnyFile)
         
         
 		
-        if dlg.exec_():
-            filenames = dlg.selectedFiles()
-            filename = filenames[0]
-            self.init_playlist(filename)
+        # if dlg.exec_():
+        #     filenames = dlg.selectedFiles()
+        #     filename = filenames[0]
+        #     self.init_playlist(filename)
 			
         # with f:
         #     data = f.read()
         #     self.contents.setText(data)
+
+    #上一首
+    def prev(self):
+        self.playlist.previous()
+        #self.currentIndex = self.playlist.previousIndex()
+        #self.playlist.setCurrentIndex(self.currentIndex)
+        self.player.play()
+
+    #下一首
+    def next(self):
+        self.playlist.next()
+        #self.currentIndex = self.playlist.nextIndex()
+        #self.playlist.setCurrentIndex(self.currentIndex)
+        self.player.play()
+        pass
+   
+
+    def clickedlist(self, qModelIndex):
+        #QMessageBox.information(self, "QListView", "你选择了: "+ self.qList[qModelIndex.row()])
+        index = qModelIndex.row()  
+        self.currentIndex = index+1
+        self.playlist.setCurrentIndex(self.currentIndex)
+        self.player.play()
+        print("点击的是：" + str(qModelIndex.row()))
+    
 
     
 
